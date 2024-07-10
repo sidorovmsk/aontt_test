0f149bda4a6f3640eb84775d71238f6b7a2ed544
a.v.sidorov <a.v.sidorov@itfbgroup.ru>
2024-07-10 12:14:43 +0300

    init

---
+++ src/main/java/org/example/aontt_test/AonttTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.aontt_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AonttTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AonttTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aontt_test/AonttTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aontt_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AonttTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
